d6058f0b6a6541e884eae1e290bc7f0a182169c4
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-05-20 13:16:38 +0100

    Created a title, an input and a search icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
+import search from "./search.svg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App w-full px-40 py-32 bg-[#F6C6A1] text-[#0F322E]">
+    <div className="input-div">
+      <h2 className='text-3xl font-bold text-center'>Search for a word</h2>
+      <span className='flex justify-between relative'>
+            <input className='relative w-full rounded-md py-4 px-2'  type="text" placeholder='Search for a word...' />
+            <img  className='absolute top-4 cursor-pointer right-8 z-10 w-8' src={search} alt='search icon' />
+          </span>
+      
+    </div>
     </div>
   );
 }
